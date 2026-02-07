20efc74fee479ddb0eaaf115fb66da563b288ea5
郑好 <16025137+Rubyzheng@user.noreply.gitee.com>
2026-02-08 02:19:21 +0800

    更改了图片呈现路径

--- config/cdn.ts
+++ config/cdn.ts
@@ -6,13 +6,23 @@
  * 推荐：微信云开发、腾讯云 COS、阿里云 OSS、七牛云 等
  */
 
-export const CDN_FOLDER_IMAGES = [
-  '', // folder-1.png
-  '', // folder-2.png
-  '', // folder-3.png
-  '', // folder-4.png
-  '', // folder-5.png
-  ''  // folder-6.png
+const USE_CDN = true;
+
+const GITHUB_USER = 'Rubyzheng16';
+const GITHUB_REPO = 'cookies-house';
+const GITHUB_BRANCH = 'master';
+const IMAGES_PATH = 'assets/images';
+
+const CDN_BASE = `https://cdn.jsdelivr.net/gh/${GITHUB_USER}/${GITHUB_REPO}@${GITHUB_BRANCH}/${IMAGES_PATH}`;
+
+const CDN_FOLDERS = [
+  `${CDN_BASE}/folders/folder-1.png`,
+  `${CDN_BASE}/folders/folder-2.png`,
+  `${CDN_BASE}/folders/folder-3.png`,
+  `${CDN_BASE}/folders/folder-4.png`,
+  `${CDN_BASE}/folders/folder-5.png`,
+  `${CDN_BASE}/folders/folder-6.png`
 ];
 
-export const CDN_ADD_BTN = '';
+export const CDN_FOLDER_IMAGES = USE_CDN ? CDN_FOLDERS : ['', '', '', '', '', ''];
+export const CDN_ADD_BTN = USE_CDN ? `${CDN_BASE}/add-btn.png` : '';
